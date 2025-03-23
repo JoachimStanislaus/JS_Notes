1af20e2c330a2582e6c6307e2504ddf052425be4
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-03-23 12:55:25 +0000

    added array sort method notes

---
+++ array_sort.js
@@ -0,0 +1,9 @@
+// array sort method
+
+// Use sort method to apply a sorting function on the array
+
+const nums = [2,3,5,1,4]
+
+sortedNum = nums.sort((a,b) => a-b)
+
+console.log(sortedNum) // Output: [ 1, 2, 3, 4, 5 ]
