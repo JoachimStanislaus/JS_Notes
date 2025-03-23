8581fb6fe8ebbcb5245e1b20f29cfd4edfa82a60
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-03-23 12:45:09 +0000

    array reduce method notes

---
+++ array_reduce.js
@@ -0,0 +1,10 @@
+// Array reduce method
+
+// Array reduce method is used to iterate over an array and accumulate a single value, takes a function and a starting value
+
+const nums = [1,2,3,4,5] 
+
+const sum = nums.reduce((acc,num)=> acc + num, 0); 
+
+console.log(sum) // Output: 15
+
